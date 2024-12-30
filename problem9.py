1771ae537e686a97b67702d537c734f2f8b2b65d
Zane Lee <zanelee@Zanes-MacBook-Pro-5.local>
2024-12-30 15:39:45 -0800

    problem 9

---
+++ problem9.py
@@ -0,0 +1,6 @@
+# Find the product a*b*c of the Pythagorean triplet (a^2 + b^2 = c^2) where a + b + c = 1000
+
+# Did this one without a computer- instead used Euclids formula for generating
+# Pythagorean triples and computed the values a = 200, b = 375 and c = 425.
+
+print(f'Product: {200 * 375 * 425}')
